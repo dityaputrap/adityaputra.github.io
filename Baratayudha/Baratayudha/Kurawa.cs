1474b9051265a62698382765e43322ef584b0540
dityaputrap <91526569+dityaputrap@users.noreply.github.com>
2021-10-13 21:09:54 +0700

    Add files via upload

---
+++ Baratayudha/Baratayudha/Kurawa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baratayudha
+{
+    class Kurawa
+    {
+        public void hit()
+        {
+            Console.WriteLine("Kurawa is hitting");
+        }
+        public void kick()
+        {
+            Console.WriteLine("Kurawa is kick");
+        }
+        public void defend()
+        {
+            Console.WriteLine("Kurawa is defend");
+        }
+        public void dead()
+        {
+            Console.WriteLine("Kurawa is dead");
+        }
+    }
+}
